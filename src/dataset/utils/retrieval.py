7e1dedd6dc727599fd5e04595acb38f3858bd04b
Denis Fouchard <dffouchard@gmail.com>
2024-10-22 15:18:03 +0200

    update

--- src/dataset/utils/retrieval.py
+++ src/dataset/utils/retrieval.py
@@ -4,16 +4,29 @@ from pcst_fast import pcst_fast
 from torch_geometric.data.data import Data
 
 
-def retrieval_via_pcst(graph, q_emb, textual_nodes, textual_edges, topk=3, topk_e=3, cost_e=0.5):
+def retrieval_via_pcst(
+    graph, q_emb, textual_nodes, textual_edges, topk=3, topk_e=3, cost_e=0.5
+):
     c = 0.01
     if len(textual_nodes) == 0 or len(textual_edges) == 0:
-        desc = textual_nodes.to_csv(index=False) + '\n' + textual_edges.to_csv(index=False, columns=['src', 'edge_attr', 'dst'])
-        graph = Data(x=graph.x, edge_index=graph.edge_index, edge_attr=graph.edge_attr, num_nodes=graph.num_nodes)
+        desc = (
+            textual_nodes.to_csv(index=False)
+            + "\n"
+            + textual_edges.to_csv(
+                index=False, columns=["src", "edge_attr", "dst"]
+            )
+        )
+        graph = Data(
+            x=graph.x,
+            edge_index=graph.edge_index,
+            edge_attr=graph.edge_attr,
+            num_nodes=graph.num_nodes,
+        )
         return graph, desc
 
     root = -1  # unrooted
     num_clusters = 1
-    pruning = 'gw'
+    pruning = "gw"
     verbosity_level = 0
     if topk > 0:
         n_prizes = torch.nn.CosineSimilarity(dim=-1)(q_emb, graph.x)
@@ -34,11 +47,11 @@ def retrieval_via_pcst(graph, q_emb, textual_nodes, textual_edges, topk=3, topk_
         last_topk_e_value = topk_e
         for k in range(topk_e):
             indices = e_prizes == topk_e_values[k]
-            value = min((topk_e-k)/sum(indices), last_topk_e_value)
+            value = min((topk_e - k) / sum(indices), last_topk_e_value)
             e_prizes[indices] = value
-            last_topk_e_value = value*(1-c)
+            last_topk_e_value = value * (1 - c)
         # reduce the cost of the edges such that at least one edge is selected
-        cost_e = min(cost_e, e_prizes.max().item()*(1-c/2))
+        cost_e = min(cost_e, e_prizes.max().item() * (1 - c / 2))
     else:
         e_prizes = torch.zeros(graph.num_edges)
 
@@ -67,10 +80,12 @@ def retrieval_via_pcst(graph, q_emb, textual_nodes, textual_edges, topk=3, topk_
     prizes = np.concatenate([n_prizes, np.array(vritual_n_prizes)])
     num_edges = len(edges)
     if len(virtual_costs) > 0:
-        costs = np.array(costs+virtual_costs)
-        edges = np.array(edges+virtual_edges)
+        costs = np.array(costs + virtual_costs)
+        edges = np.array(edges + virtual_edges)
 
-    vertices, edges = pcst_fast(edges, prizes, costs, root, num_clusters, pruning, verbosity_level)
+    vertices, edges = pcst_fast(
+        edges, prizes, costs, root, num_clusters, pruning, verbosity_level
+    )
 
     selected_nodes = vertices[vertices < graph.num_nodes]
     selected_edges = [mapping_e[e] for e in edges if e < num_edges]
@@ -78,14 +93,22 @@ def retrieval_via_pcst(graph, q_emb, textual_nodes, textual_edges, topk=3, topk_
     if len(virtual_vertices) > 0:
         virtual_vertices = vertices[vertices >= graph.num_nodes]
         virtual_edges = [mapping_n[i] for i in virtual_vertices]
-        selected_edges = np.array(selected_edges+virtual_edges)
+        selected_edges = np.array(selected_edges + virtual_edges)
 
     edge_index = graph.edge_index[:, selected_edges]
-    selected_nodes = np.unique(np.concatenate([selected_nodes, edge_index[0].numpy(), edge_index[1].numpy()]))
+    selected_nodes = np.unique(
+        np.concatenate(
+            [selected_nodes, edge_index[0].numpy(), edge_index[1].numpy()]
+        )
+    )
 
     n = textual_nodes.iloc[selected_nodes]
     e = textual_edges.iloc[selected_edges]
-    desc = n.to_csv(index=False)+'\n'+e.to_csv(index=False, columns=['src', 'edge_attr', 'dst'])
+    desc = (
+        n.to_csv(index=False)
+        + "\n"
+        + e.to_csv(index=False, columns=["src", "edge_attr", "dst"])
+    )
 
     mapping = {n: i for i, n in enumerate(selected_nodes.tolist())}
 
@@ -94,6 +117,11 @@ def retrieval_via_pcst(graph, q_emb, textual_nodes, textual_edges, topk=3, topk_
     src = [mapping[i] for i in edge_index[0].tolist()]
     dst = [mapping[i] for i in edge_index[1].tolist()]
     edge_index = torch.LongTensor([src, dst])
-    data = Data(x=x, edge_index=edge_index, edge_attr=edge_attr, num_nodes=len(selected_nodes))
+    data = Data(
+        x=x,
+        edge_index=edge_index,
+        edge_attr=edge_attr,
+        num_nodes=len(selected_nodes),
+    )
 
     return data, desc
--- train.py
+++ train.py
@@ -22,9 +22,11 @@ def main(args):
 
     # Step 1: Set up wandb
     seed = args.seed
-    wandb.init(project=f"{args.project}",
-               name=f"{args.dataset}_{args.model_name}_seed{seed}",
-               config=args)
+    wandb.init(
+        project=f"{args.project}",
+        name=f"{args.dataset}_{args.model_name}_seed{seed}",
+        config=args,
+    )
 
     seed_everything(seed=args.seed)
     print(args)
@@ -33,36 +35,63 @@ def main(args):
     idx_split = dataset.get_idx_split()
 
     # Step 2: Build Node Classification Dataset
-    train_dataset = [dataset[i] for i in idx_split['train']]
-    val_dataset = [dataset[i] for i in idx_split['val']]
-    test_dataset = [dataset[i] for i in idx_split['test']]
+    train_dataset = [dataset[i] for i in idx_split["train"]]
+    val_dataset = [dataset[i] for i in idx_split["val"]]
+    test_dataset = [dataset[i] for i in idx_split["test"]]
 
-    train_loader = DataLoader(train_dataset, batch_size=args.batch_size, drop_last=True, pin_memory=True, shuffle=True, collate_fn=collate_fn)
-    val_loader = DataLoader(val_dataset, batch_size=args.batch_size, drop_last=False, pin_memory=True, shuffle=False, collate_fn=collate_fn)
-    test_loader = DataLoader(test_dataset, batch_size=args.eval_batch_size, drop_last=False, pin_memory=True, shuffle=False, collate_fn=collate_fn)
+    train_loader = DataLoader(
+        train_dataset,
+        batch_size=args.batch_size,
+        drop_last=True,
+        pin_memory=True,
+        shuffle=True,
+        collate_fn=collate_fn,
+    )
+    val_loader = DataLoader(
+        val_dataset,
+        batch_size=args.batch_size,
+        drop_last=False,
+        pin_memory=True,
+        shuffle=False,
+        collate_fn=collate_fn,
+    )
+    test_loader = DataLoader(
+        test_dataset,
+        batch_size=args.eval_batch_size,
+        drop_last=False,
+        pin_memory=True,
+        shuffle=False,
+        collate_fn=collate_fn,
+    )
 
     # Step 3: Build Model
     args.llm_model_path = llama_model_path[args.llm_model_name]
-    model = load_model[args.model_name](graph_type=dataset.graph_type, args=args, init_prompt=dataset.prompt)
+    model = load_model[args.model_name](
+        graph_type=dataset.graph_type, args=args, init_prompt=dataset.prompt
+    )
 
     # Step 4 Set Optimizer
     params = [p for _, p in model.named_parameters() if p.requires_grad]
     optimizer = torch.optim.AdamW(
-        [{'params': params, 'lr': args.lr, 'weight_decay': args.wd}, ],
-        betas=(0.9, 0.95)
+        [
+            {"params": params, "lr": args.lr, "weight_decay": args.wd},
+        ],
+        betas=(0.9, 0.95),
     )
     trainable_params, all_param = model.print_trainable_params()
-    print(f"trainable params: {trainable_params} || all params: {all_param} || trainable%: {100 * trainable_params / all_param}")
+    print(
+        f"trainable params: {trainable_params} || all params: {all_param} || trainable%: {100 * trainable_params / all_param}"
+    )
 
     # Step 5. Training
     num_training_steps = args.num_epochs * len(train_loader)
     progress_bar = tqdm(range(num_training_steps))
-    best_val_loss = float('inf')
+    best_val_loss = float("inf")
 
     for epoch in range(args.num_epochs):
 
         model.train()
-        epoch_loss, accum_loss = 0., 0.
+        epoch_loss, accum_loss = 0.0, 0.0
 
         for step, batch in enumerate(train_loader):
 
@@ -70,54 +99,66 @@ def main(args):
             loss = model(batch)
             loss.backward()
 
-            clip_grad_norm_(optimizer.param_groups[0]['params'], 0.1)
+            clip_grad_norm_(optimizer.param_groups[0]["params"], 0.1)
 
             if (step + 1) % args.grad_steps == 0:
-                adjust_learning_rate(optimizer.param_groups[0], args.lr, step / len(train_loader) + epoch, args)
+                adjust_learning_rate(
+                    optimizer.param_groups[0],
+                    args.lr,
+                    step / len(train_loader) + epoch,
+                    args,
+                )
 
             optimizer.step()
-            epoch_loss, accum_loss = epoch_loss + loss.item(), accum_loss + loss.item()
+            epoch_loss, accum_loss = (
+                epoch_loss + loss.item(),
+                accum_loss + loss.item(),
+            )
 
             if (step + 1) % args.grad_steps == 0:
                 lr = optimizer.param_groups[0]["lr"]
-                wandb.log({'Lr': lr})
-                wandb.log({'Accum Loss': accum_loss / args.grad_steps})
-                accum_loss = 0.
+                wandb.log({"Lr": lr})
+                wandb.log({"Accum Loss": accum_loss / args.grad_steps})
+                accum_loss = 0.0
 
             progress_bar.update(1)
 
-        print(f"Epoch: {epoch}|{args.num_epochs}: Train Loss (Epoch Mean): {epoch_loss / len(train_loader)}")
-        wandb.log({'Train Loss (Epoch Mean)': epoch_loss / len(train_loader)})
+        print(
+            f"Epoch: {epoch}|{args.num_epochs}: Train Loss (Epoch Mean): {epoch_loss / len(train_loader)}"
+        )
+        wandb.log({"Train Loss (Epoch Mean)": epoch_loss / len(train_loader)})
 
-        val_loss = 0.
+        val_loss = 0.0
         eval_output = []
         model.eval()
         with torch.no_grad():
             for step, batch in enumerate(val_loader):
                 loss = model(batch)
                 val_loss += loss.item()
-            val_loss = val_loss/len(val_loader)
+            val_loss = val_loss / len(val_loader)
             print(f"Epoch: {epoch}|{args.num_epochs}: Val Loss: {val_loss}")
-            wandb.log({'Val Loss': val_loss})
+            wandb.log({"Val Loss": val_loss})
 
         if val_loss < best_val_loss:
             best_val_loss = val_loss
             _save_checkpoint(model, optimizer, epoch, args, is_best=True)
             best_epoch = epoch
 
-        print(f'Epoch {epoch} Val Loss {val_loss} Best Val Loss {best_val_loss} Best Epoch {best_epoch}')
+        print(
+            f"Epoch {epoch} Val Loss {val_loss} Best Val Loss {best_val_loss} Best Epoch {best_epoch}"
+        )
 
         if epoch - best_epoch >= args.patience:
-            print(f'Early stop at epoch {epoch}')
+            print(f"Early stop at epoch {epoch}")
             break
 
     torch.cuda.empty_cache()
     torch.cuda.reset_max_memory_allocated()
 
     # Step 5. Evaluating
-    os.makedirs(f'{args.output_dir}/{args.dataset}', exist_ok=True)
-    path = f'{args.output_dir}/{args.dataset}/model_name_{args.model_name}_llm_model_name_{args.llm_model_name}_llm_frozen_{args.llm_frozen}_max_txt_len_{args.max_txt_len}_max_new_tokens_{args.max_new_tokens}_gnn_model_name_{args.gnn_model_name}_patience_{args.patience}_num_epochs_{args.num_epochs}_seed{seed}.csv'
-    print(f'path: {path}')
+    os.makedirs(f"{args.output_dir}/{args.dataset}", exist_ok=True)
+    path = f"{args.output_dir}/{args.dataset}/model_name_{args.model_name}_llm_model_name_{args.llm_model_name}_llm_frozen_{args.llm_frozen}_max_txt_len_{args.max_txt_len}_max_new_tokens_{args.max_new_tokens}_gnn_model_name_{args.gnn_model_name}_patience_{args.patience}_num_epochs_{args.num_epochs}_seed{seed}.csv"
+    print(f"path: {path}")
 
     model = _reload_best_model(model, args)
     model.eval()
@@ -133,8 +174,8 @@ def main(args):
 
     # Step 6. Post-processing & compute metrics
     acc = eval_funcs[args.dataset](path)
-    print(f'Test Acc {acc}')
-    wandb.log({'Test Acc': acc})
+    print(f"Test Acc {acc}")
+    wandb.log({"Test Acc": acc})
 
 
 if __name__ == "__main__":
